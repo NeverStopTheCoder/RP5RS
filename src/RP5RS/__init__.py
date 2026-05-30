7d01cd6ea550e1359e900cf6bde47be553be746f
NeverStopTheCoder <jenny_fullerton@yahoo.com>
2026-05-30 13:19:41 -0400

    Update __init__.py

--- src/RP5RS/__init__.py
+++ src/RP5RS/__init__.py
@@ -1,5 +1,5 @@
 # Pull the functions from your sub-file straight to the surface
-from .RP5RS import SetCurrentGameTo, RunGame, SendGameOver
+from .RP5RS import SetCurrentGameTo, RunGame, SendGameOver, ReplaceAllFilesWith
 
 # (Optional) This tells Python exactly what is allowed to be exported
-__all__ = ['SetCurrentGameTo', 'RunGame']
+__all__ = ['SetCurrentGameTo', 'RunGame', 'SendGameOver', 'ReplaceAllFilesWith']
